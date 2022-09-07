34f5f1b4075ed2edaa074c5a6861e6deb59794c6
Martin Mayer <ms-mayer@outlook.de>
2022-09-07 13:01:56 -0700

    fix: changed default location for deployment-scripts
    Changed from us to europe-west1

--- delete-account/deploy-dev.bat
+++ delete-account/deploy-dev.bat
@@ -2,4 +2,5 @@ gcloud functions deploy DeleteUserData ^
 --trigger-event providers/firebase.auth/eventTypes/user.delete ^
 --trigger-resource linum-dev ^
 --source src ^
+--region europe-west1 ^
 --runtime go116
--- delete-account/deploy.sh
+++ delete-account/deploy.sh
@@ -2,4 +2,5 @@ gcloud functions deploy DeleteUserData \
 --trigger-event providers/firebase.auth/eventTypes/user.delete \
 --trigger-resource linum-5d9f6 \
 --source src \
+--region europe-west1 \
 --runtime go116
